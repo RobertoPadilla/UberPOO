a9b742fdb88cce95007f60701cd8c2d889461bfa
Roberto Padilla <basshivan981@gmail.com>
2020-02-06 19:55:40 -0600

    This is crazy! One object passed to another object

--- JAVA/Car.java
+++ JAVA/Car.java
@@ -1,10 +1,17 @@
 class Car {
   Integer id;
   String license;
-  String driver;
+  Account driver;
   Integer passenger;
 
+  // Constructor, cosas con las que sin ellas, el objeto no puede vivir
+
+  public Car(String license, Account driver){//los nombres de variables pueden ser los que queramos y son variables locales
+    this.license = license; //this nos permite acceder a todos los atributos de esta clase, por ejemplo, driver.
+    this.driver = driver;
+
+  }
   void printDataCar() {
-    System.out.println("License: " + license + " Driver: " + driver);
+    System.out.println("License: " + license + " Driver: " + driver.name);
   }
 }
--- JAVA/Main.java
+++ JAVA/Main.java
@@ -1,10 +1,11 @@
 class Main {
   public static void main(String[] args) {
     System.out.println("Hola Mundo");
-    Car car = new Car();
-    car.license = "AMQ123";
-    car.driver = "Andrés Herrera";
+    Car car = new Car("AMQ123", new Account("Andres Herrera", "AND123"));
     car.passenger = 4;
+    Car car2 = new Car("QWE123", new Account("Andrea Herrera", "ANA123"));
+    car.passenger = 3;
     car.printDataCar();
+    car2.printDataCar();
   }
 }

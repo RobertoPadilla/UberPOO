7b0c5226ad9a697e5b26040dc6c880a81c8eaae4
RobertoPadilla <basshivan981@gmail.com>
2020-02-09 09:56:38 -0600

    Here I made mi first constructor in pyhton and I tryed to implements gitignore

--- PYTHON/main.py
+++ PYTHON/main.py
@@ -1,13 +1,18 @@
 from Car import Car
+from Account import Account
 
 if __name__ == "__main__":
   print("Hola Mundo")
-  car = Car()
-  car.license = "AMS234"
-  car.driver = "Andres Herrera"
-  print(vars(car))
+  # car = Car()
+  # car.license = "AMS234"
+  # car.driver = "Andres Herrera"
+  # print(vars(car))
 
-  car2 = Car()
-  car2.license = "DWE342"
-  car2.driver = "Andrea Lopez"
-  print(vars(car2))
+  # car2 = Car()
+  # car2.license = "DWE342"
+  # car2.driver = "Andrea Lopez"
+  # print(vars(car2))
+
+  car = Car("AMS234", Account("Andres Herrera", "AND123"))
+  print(vars(car))
+  print(vars(car.driver))

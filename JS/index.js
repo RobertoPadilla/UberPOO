efa16a70db006b9712c1069d130aa8040e96aef6
RobertoPadilla <basshivan981@gmail.com>
2020-02-09 12:46:10 -0600

    My Uber classes

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,9 @@
-var car = new Car("AW456", new Account("Andres Herrera", "AND123"))
-car.passenger = 4;
-car.printDataCar();
+// var car = new Car("AW456", new Account("Andres Herrera", "AND123"))
+// car.passenger = 4;
+// car.printDataCar();
+
+// Con Herencia
+
+var uberX = new UberX("UBX123", new Account("Andrea Ferran", "AND123"), "Chevrolet", "Spark");
+uberX.passenger = 4;
+uberX.printDataCar();
